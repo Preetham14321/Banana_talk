4700b3f1e2a60983aa8ab3b10219a12dd8ff9ea7
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2021-03-13 15:14:30 +0530

    comit

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#translate");
+var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
+// outputDiv.innerText = "ho";
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+  outputDiv.innerText = "ajfjjjjajjgj" + " " + txtInput.value;
+});
